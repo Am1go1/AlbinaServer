c0435644e10bbf09a1b62e5595cc708ea2224e53
StudentDev <vovaklinov007@gmail.com>
2021-11-09 19:24:43 +0300

    fff
    fff

---
+++ Albina/Albina.DataAccess.Core/Models/UserRto.cs
@@ -0,0 +1,24 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Albina.DataAccess.Core.Models
+{
+    [Table("User")]
+    public class UserRto
+    {
+        [Key] public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        [Required] public string PhoneNumberPrefix { get; set; }
+
+        [Required] public string PhoneNumber { get; set; }
+
+        [Required, MinLength(7)] public string Password { get; set; }
+
+        public string ImageName { get; set; }
+    }
+}
---
+++ Albina/Albina.DataAccess.Core/Models/VirtualExpenseRto.cs
@@ -0,0 +1,22 @@
+﻿using Albina.DataAccess.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Albina.DataAccess.Core.Models
+{
+    [Table("VirtualExpense")]
+    public class VirtualExpenseRto
+    {
+        [Key] public int Id { get; set; }
+        public float Cost { get; set; }
+        public CostCurrencyRto CostCurrency { get; set; }
+        public VirtualExpenseStatusRto Status { get; set; }
+        public DateTimeOffset CreateDataTime { get; set; }
+        public List<DateTimeOffset> ChangeDataTime { get; set; }
+    }
+}
